bd20a52bce8ad50baac70bac7ef982e1d61db8fa
Javier <javilopeza9@gmail.com>
2018-01-20 22:05:13 +0100

    '1 2 3'

--- installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-stow emacs
-stow i3
-stow i3status
--- update_dots.sh
+++ update_dots.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -lt 1 ]
 then
     echo "Usage: ./update_dots commitname"
     exit 1
 fi
 cd ~/dots
 git add .
-git commit -m "$1"
+git commit -m "'$*'"
 git push origin master
