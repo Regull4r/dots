1da7fb35fbeacd8eba23a039ad8740b66aed9570
Javier López <javilopeza9@gmail.com>
2018-04-24 17:12:48 +0200

    'bye swi'

--- emacs/.emacs.d/config/languagues.el
+++ emacs/.emacs.d/config/languagues.el
@@ -19,14 +19,14 @@
   :ensure t
   :config(elpy-enable))
 
-;;Prolog
-(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
-(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
-(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
-(setq prolog-system 'swi)
-(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
-                                ("\\.m$" . mercury-mode))
-			      auto-mode-alist))
-
-(use-package ediprolog
-  :ensure t)
+;;;;Prolog
+;;(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+;;(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+;;(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+;;(setq prolog-system 'swi)
+;;(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+;;                                ("\\.m$" . mercury-mode))
+;;			      auto-mode-alist))
+;;
+;;(use-package ediprolog
+;;  :ensure t)
